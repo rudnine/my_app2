5b3c28eb75ca14518620230f4e30045034ae8be8
rudnine <st3yohan@gmail.com>
2016-08-12 01:01:33 +0900

    create css

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+/*
 app.get('/',function(req,res){
     res.send('Hello ks world!');
-});
+});*/
+
+//app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function(){
     console.log('Server On!!');
