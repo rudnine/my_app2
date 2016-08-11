44541f25d19a10f0298f3b9b690af0ed2b5c2102
rudnine <st3yohan@gmail.com>
2016-08-12 00:43:02 +0900

    express setting

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res){
+    res.send('Hello ks world!');
+});
+
+app.listen(3000, function(){
+    console.log('Server On!!');
+});
+
